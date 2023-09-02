58701a45c210109c6939b2950b21823348d57342
Yuval Dikerman <3dyuval@gmail.com>
2023-09-02 15:47:22 +0300

    feed and link show body

---
+++ src/assets/utils/dates.ts
@@ -0,0 +1,13 @@
+import { formatDistance } from 'date-fns'
+
+
+export const formatDateRelative = (date: number | string) => {
+    let parsed = date
+    if (typeof parsed === 'string') {
+      parsed = Number.parseInt(parsed)
+    }
+    return formatDistance(parsed, new Date(), {
+      addSuffix: true,
+    })
+  }
+  
